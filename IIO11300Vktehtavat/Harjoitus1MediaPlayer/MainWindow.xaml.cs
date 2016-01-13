30d19f2bcb8d271ee4f74ee181e9776be1031045
laurima <lauri.m.makinen@gmail.com>
2016-01-13 11:11:14 +0200

    Ensimmäinen tehtävä, jossa video pyörii

---
+++ IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus1MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            //soitetaan käyttäjän valitsemaa mediatiedostoa
+            string filu = @"D:\H9142\SampleVideo.mp4";
+            //tutkitaan onko tiedosto olemassa
+            if (System.IO.File.Exists(filu))
+            {
+                //MessageBox.Show("Playing: " + filu);
+                mediaElement.Source = new Uri(filu);
+                mediaElement.Play();
+            }
+        }
+
+        private void btmUrl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
